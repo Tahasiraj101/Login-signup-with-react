be5b4e8238ff059f052ea5cd0a374d22fc48b237
tahasiraj101 <pikachugaming899@gmail.com>
2025-01-12 12:10:58 -0800

    Updated App.jsx and deleted unused files

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,26 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { GlobalContext } from "./Context/Context"; // Import Context
 import Login from "./Login";
 import Home from "./Home";
-import ProductDetails from "./ProductDetails";
-import ContextProvider from "./Context/Context"; 
+import ProductDetails from "./ProductDetails"; // Import ProductDetails
 
 function App() {
-  const { state } = useContext(GlobalContext); 
+  const { state } = useContext(GlobalContext); // Use context state
 
   return (
-<<<<<<< HEAD
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard Route */}
-      </Routes>
+      {state.isLogin ? (
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      )}
     </Router>
-=======
-    <ContextProvider>  
-      <Router>
-        {state.isLogin ? (
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="*" element={<Navigate to="/home" />} />
-          </Routes>
-        ) : (
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-        )}
-      </Router>
-    </ContextProvider>
->>>>>>> 8df64d9 (login-api)
   );
 }
 
